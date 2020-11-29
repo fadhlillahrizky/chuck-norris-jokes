0d6c40dd568a8bd8f0b03cdbf5440ecddd3206f1
fadhlillahrizky <rizky.fadhlillah3@gmail.com>
2020-11-29 19:24:02 +0700

    test driven jokes

--- tests/JokeFactoryTest.php
+++ tests/JokeFactoryTest.php
@@ -4,6 +4,7 @@
 namespace Fadhlillah25\ChuckNorrisJokes\Tests;
 
 
+use Fadhlillah25\ChuckNorrisJokes\JokeFactory;
 use PHPUnit\Framework\TestCase;
 
 class JokeFactoryTest extends TestCase
@@ -11,6 +12,26 @@ class JokeFactoryTest extends TestCase
     /** @test */
     public function  it_returns_a_random_joke()
     {
-        $this->assertTrue(true);
+        $jokes = new JokeFactory([
+                'This is a joke',
+            ]);
+        $joke = $jokes->getRandomJoke();
+
+        $this->assertSame('This is a joke', $joke);
+    }
+
+    /** @test */
+    public function  it_returns_a_predefined_joke()
+    {
+        $chuckNorrisJokes = [
+            'The First rule of Chuck Norris is: you do not talk about Chuck Norris.',
+            'Chuck Norris counted to infinity... Twice.',
+            'If you can see Chuck Norris, he can see you. If you can\'t see Chuck Norris you may be only seconds away from death.'
+        ];
+
+        $jokes = new JokeFactory();
+        $joke = $jokes->getRandomJoke();
+
+        $this->assertContains($joke, $chuckNorrisJokes);
     }
 }
